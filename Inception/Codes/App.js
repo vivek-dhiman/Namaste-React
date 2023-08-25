61c0619b8bba70e5d040b3c8b357023afaa213dd
Vivek <vivekdhiman1208@gmail.com>
2023-08-26 01:08:31 +0530

    Class 2 update

--- Inception/Codes/App.js
+++ Inception/Codes/App.js
@@ -1,8 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const heading1 = React.createElement("h1", {}, "Namaste Everyone From React!");
 const heading2 = React.createElement(
   "h1",
   {},
-  "Namaste Everyone From React 2!"
+  "Namaste Everyone From Parcel 2!"
 );
 const container = React.createElement("div", {}, [heading1, heading2]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
